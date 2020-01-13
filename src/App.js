717ce4f159409e7c926b95b62422b2a9438fb0ab
Andre <andregpilakis@gmail.com>
2020-01-13 14:21:37 +0000

    basic API calling created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+//We begin on port 3000.
+import React,{useEffect, useState} from 'react'; 
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
-
   const APP_ID = 'b94e9bb3';
   const APP_KEY = '550543cb72809cae78bbd3cedf40b168';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  const [recipes, setRecipes] = useState([]);
 
+useEffect( () =>{
+  getRecipes();
+}, []);
+
+const getRecipes = async () =>{
+  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const data = await response.json();
+
+  setRecipes(data.hits);
+}
 
 return(
-<div className="App">
-  <h1>Hello react</h1>
-</div>
-);
+  <div className="App">
+    <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">
+        search
+        </button> 
+    </form>
+    
+  </div>
+  );
 };
 
 export default App;
+
+
+//<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
