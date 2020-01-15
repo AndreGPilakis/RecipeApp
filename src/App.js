fccbbd44cd757013523adfe5d4aa5c89175b6971
Andre <andregpilakis@gmail.com>
2020-01-15 06:42:24 +0000

    Can now search for different food items

--- src/App.js
+++ src/App.js
@@ -7,28 +7,53 @@ const App = () => {
   const APP_ID = 'b94e9bb3';
   const APP_KEY = '550543cb72809cae78bbd3cedf40b168';
 
-  const [recipes, setRecipes] = useState([]);
+ const [recipes, setRecipes] = useState([]);
+ const [search, setSearch] = useState('');
+ const [query, setQuery] = useState("chicken");
 
+
+//Not running API call on each key press as it will flood API requiests
 useEffect( () =>{
   getRecipes();
-}, []);
+}, [query]);
 
 const getRecipes = async () =>{
-  const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
   const data = await response.json();
 
   setRecipes(data.hits);
+  console.log(data.hits);
 }
+
+//Updates search with the events target, which is the text field.
+const updateSearch = e => {
+  setSearch(e.target.value);
+}
+
+const getSearch = e => {
+  //stops the default of refreshing the page
+  e.preventDefault();
+  setQuery(search);
+  setSearch('');
+}
+
 
 return(
   <div className="App">
-    <form className="search-form">
-      <input className="search-bar" type="text"/>
+    <form onSubmit={getSearch} className="search-form">
+      <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
       <button className="search-button" type="submit">
         search
         </button> 
     </form>
-    
+    {recipes.map(recipe => (
+      //We use recipe.recipe because the array is the same name as our var.
+      <Recipe 
+      key={recipe.recipe.label}
+      title={recipe.recipe.label} 
+      calories={recipe.recipe.calories} 
+      image={recipe.recipe.image}/>
+    ))};
   </div>
   );
 };
